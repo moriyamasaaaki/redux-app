8098c386445f12598f9b25be64e96ce57d4ad155
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-18 17:32:15 +0900

    feat(json,index.js): redux-devtool導入 #10

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ import EventsNew from "./components/events_new";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-const store = createStore(reducer, applyMiddleware(thunk));
+import { composeWithDevTools } from 'redux-devtools-extension';
+const enhancer =
+  process.env.NODE_ENV === "development"
+    ? composeWithDevTools(applyMiddleware(thunk))
+    : applyMiddleware(thunk);
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
