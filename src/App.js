9b8089df3427c5300b9150050b77161295af9030
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-18 12:04:38 +0900

    feat(App): count実装

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'; 
+const App = () => (
+  <Counter></Counter>
+)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0}
+  }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  handleMinasuButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>Count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinasuButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
 export default App;
