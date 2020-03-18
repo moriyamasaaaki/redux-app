2a008e9b7fde735961416a1d8023e0947ac76e7b
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-18 17:20:53 +0900

    feat(events_new): buttonにdisabled追加 #8

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -25,7 +25,7 @@ class EventsNew extends Component {
       this.props.history.push('/')
   }
   render() {
-    const {handleSubmit} = this.props
+    const { handleSubmit, pristine, submitting } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -45,8 +45,12 @@ class EventsNew extends Component {
           />
         </div>
         <div>
-            <input type="submit" value="Submit" disabled={false} />
-            <Link to="/">Cancel</Link>
+          <input
+            type="submit"
+            value="Submit"
+            disabled={pristine || submitting}
+          />
+          <Link to="/">Cancel</Link>
         </div>
       </form>
     );
