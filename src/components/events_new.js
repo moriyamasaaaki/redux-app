9a0a3be2aac1be99de0b89cc3ddd38517b6c426c
moriyamasaaaki <moriya-7071@outlook.com>
2020-03-18 16:33:58 +0900

    feat(eventNew): ページ作成、redux-form導入 #6

---
+++ src/components/events_new.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+// import { postEvents } from "../actions";
+import { Link } from "react-router-dom";
+class EventsNew extends Component {
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error }
+    } = field;
+    return <div></div>;
+  }
+  render() {
+    return (
+      <form>
+        <div>
+          <Field
+            label="Title"
+            name="title"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
+        <div>
+          <Field
+            label="Body"
+            name="body"
+            type="text"
+            component={this.renderField}
+          />
+        </div>
+        <div>
+            <input type="submit" value="Submit" disabled={false} />
+            <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    );
+  }
+}
+
+const validate = values => {
+    const errors = {}
+    return errors
+}
+
+// const mapDispatchToProps = { postEvents };
+
+export default connect(null, null)(
+    reduxForm({ validate, form: 'eventNewForm'})
+(EventsNew));
--- src/index.js
+++ src/index.js
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 import reducer from "./reducers";
 import EventsIndex from "./components/events_index";
+import EventsNew from "./components/events_new";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-<Provider store={store}>
-    <EventsIndex />
-</Provider>,
-document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={ EventsNew } />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
